eb6e1e6557bfdd9aa18c85adb3073398734008b8
NickNaso <nicoladelgobbo@gmail.com>
2016-05-08 17:39:23 +0200

    Update for 1.0.3 version

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+
+# Changelog for **response-powered-by**
+
+1.0.3 / 2016-05-08
+==================
+
+  * Update comment in the code
+  * Update auhtor and contributors for the project
+  * Update `package.json`
+  * Added test suite for middleware based on [jasmine](http://jasmine.github.io/edge/introduction.html)
+  * Update documentation comment
+  * Renamed folder libs to lib
+  * Added .jshintrc file that helps to detect errors and potential problems in your JavaScript code through [jshint](http://jshint.com/docs/)
+
+1.0.2 / 2015-08-11
+==================
+
+  * Add missing elements to `package.json`
+
+1.0.1 / 2015-08-11
+==================
+
+  * Change documentation for bug [Error in Readme.md #1](https://github.com/NickNaso/response-powered-by/issues/1)
+
+1.0.0 / 2015-08-03
+==================
+
+  * Initial version of `response-powered-by` middleware
---
+++ test/response-powered-by.js
@@ -0,0 +1,80 @@
+/******************************************************************************
+ * Copyright (c) 2016 Nicola Del Gobbo
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+ * WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+ * MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache Version 2.0 License for specific language governing permissions
+ * and limitations under the License.
+ *
+ * Contributors - initial API implementation:
+ * Nicola Del Gobbo <nicoladelgobbo@gmail.com>
+ * Antonio D'Angelo <tonydangelo123@gmail.com>
+ *****************************************************************************/
+
+'use strict';
+
+var express = require('express');
+var request = require('supertest');
+var responsePoweredBy = require('../');
+
+describe("Test response-powered-by() middleware", function(){
+
+  it("Should export function", function(){
+
+		  expect(typeof responsePoweredBy).toEqual('function');
+
+	});
+
+  it("Should remove `X-Powered-By` header set by Express", function(done){
+
+    var app = express()
+      .use(responsePoweredBy())
+      .get("/", function(req, res){
+        res.send("Response Powered By");
+      });
+
+    request(app)
+      .get("/")
+      .expect(200)
+      .end(function(err, res){
+      if(err) {
+        done.fail(err);
+      } else {
+        expect(res.get('X-Powered-By')).toEqual(undefined);
+        done();
+      }
+    });
+
+  });
+
+  it("Should remove and set `X-Powered-By` header set by Express", function(done){
+
+    var xPoweredBy = "@NickNaso";
+
+    var app = express()
+      .use(responsePoweredBy(xPoweredBy))
+      .get("/", function(req, res){
+        res.send("Response Powered By");
+      });
+
+    request(app)
+      .get("/")
+      .expect(200)
+      .end(function(err, res){
+      if(err) {
+        done.fail(err);
+      } else {
+        expect(res.get('X-Powered-By')).toEqual(xPoweredBy);
+        done();
+      }
+    });
+
+  });
+
+});
